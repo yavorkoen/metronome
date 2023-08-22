34e01f735589ebc2087a5bc5d4386c6275986673
Yavor Koen <yavorkoen@gmail.com>
2023-08-22 11:37:08 +0300

    add change speed function

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 console.log('hello metronome');
-let interval;
 
 let btn = document.getElementById('play-btn');
+let tempoInput = document.getElementById('tempo');
+
+let tempo = tempoInput.value;
+let ratio = tempo / 60;
+
+let interval;
+
 btn.addEventListener('click', toggleMetronome);
+tempoInput.addEventListener('change', changeTempo);
+
 function toggleMetronome(e) {
     console.log(e.target.id);
     if (e.target.id === 'play-btn') {
-        let time = 1000;
-        interval = setInterval(playSound, time);
+        interval = setInterval(playSound, setTempo());
         playSound();
         e.target.id = 'stop-btn';
         e.target.textContent = 'stop';
@@ -23,4 +30,18 @@ function toggleMetronome(e) {
 function playSound() {
     const audio = new Audio('clave-sound.mp3');
     audio.play();
+}
+function getTempo(e) {
+    tempo = e.target.value;
+    ratio = tempo/60;
+}
+function setTempo() {
+    return 1000 / ratio;
+}
+function changeTempo(e) {
+    tempo = e.target.value;
+    ratio = tempo/60;
+    clearInterval(interval);
+    interval = null;
+    interval = setInterval(playSound, setTempo());
 }
