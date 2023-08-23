c2f8f2ad97d3b8584b21c6c28462216661e2d0af
Yavor Koen <yavorkoen@gmail.com>
2023-08-23 23:09:42 +0300

    create timer function

--- app.js
+++ app.js
@@ -1,58 +1,94 @@
+import Timer from "./timer";
+
+
 let btn = document.getElementById('play-btn');
 let tempoInput = document.getElementById('tempo');
 let noteLengthSelect = document.getElementById('note-length');
 
 let tempo = tempoInput.value;
-let ratio = tempo / 60;
+
 
 const noteLengthsRatios = {
-    'quarter': tempo / 60,
-    'eight': tempo / 60 * 2,
-    'qrt-tuple': tempo / 60 / 2 * 3,
-    'eight-tuple': tempo / 60 * 3,
+    'quarter': 1,
+    'eight': 2,
+    'qrt-tuple': 1.5,
+    'eight-tuple': 3,
 }
 
+let ratio = tempo / 60 * noteLengthsRatios[noteLengthSelect.selectedOptions[0].value]
+
+const noteLengthsBeats = {
+    'quarter': 4,
+    'eight': 2,
+    'qrt-tuple': 3,
+    'eight-tuple': 3,
+}
+
+
 let interval;
+let counter = 2;
+let beats = 4;
 
 btn.addEventListener('click', toggleMetronome);
 noteLengthSelect.addEventListener('change', setNoteLengthRatio);
 
 function toggleMetronome(e) {
-    console.log(e.target.id);
     if (e.target.id === 'play-btn') {
         e.target.id = 'stop-btn';
         e.target.textContent = 'stop';
-        interval = setInterval(playSound, setTempo());
         playSound();
+        interval = setInterval(playSoundLoop, setTempo());
         tempoInput.addEventListener('change', getChangeTempo);
-    } else if(e.target.id === 'stop-btn') {
+    } else if (e.target.id === 'stop-btn') {
         clearInterval(interval)
         interval = null;
         e.target.id = 'play-btn';
         e.target.textContent = 'play';
         tempoInput.removeEventListener('change', getChangeTempo);
+        counter = 2;
     }
 }
 
 
 function playSound() {
-    const audio = new Audio('clave-sound.mp3');
+    // console.log(tempo);
+    // console.log(ratio);
+    const audio = new Audio('clave-high.mp3');
     audio.play();
 }
+
+function playSoundLoop() {
+    console.log(beats);
+    if (counter === 1) {
+        const claveHighSound = new Audio('clave-high.mp3');
+        claveHighSound.play();
+    } else {
+        const claveSound = new Audio('clave-sound.mp3');
+        claveSound.play();
+    }
+    if (counter < beats) {
+        counter++;
+    } else {
+        counter = 1;
+    }
+    console.log(counter, 'counter');
+}
+
 function setTempo() {
+    console.log(ratio, 'setTempo');
     return 1000 / ratio;
 }
 function getChangeTempo(e) {
     tempo = e.target.value;
-    ratio = tempo/60;
+    ratio = tempo / 60 * noteLengthsRatios[noteLengthSelect.selectedOptions[0].value];
     clearInterval(interval);
     interval = null;
-    interval = setInterval(playSound, setTempo());
+    interval = setInterval(playSoundLoop, setTempo());
 }
 function setNoteLengthRatio(e) {
-    tempo = tempoInput.value;
-    ratio = noteLengthsRatios[e.target.selectedOptions[0].value];
+    ratio = tempo / 60 * noteLengthsRatios[e.target.selectedOptions[0].value];
+    beats = noteLengthsBeats[e.target.selectedOptions[0].value]
     clearInterval(interval);
     interval = null;
-    console.log(ratio);
+    interval = setInterval(playSoundLoop, setTempo());
 }
---
+++ timer.js
@@ -0,0 +1,39 @@
+function Timer(callback, timeInterval, options) {
+    this.timeInterval = timeInterval;
+    //start timer
+    this.start = () => {
+        this.expected = Date.now() + this.timeInterval;
+        this.theTimeout = null;
+
+        if(options.immediate) {
+            callback();
+        }
+
+        this.timeout = setTimeout(this.round, this.timeInterval);
+        console.log('Started!');
+    }
+    //stop timer
+    this.stop = () => {
+
+        clearTimeout(this.timeout);
+        console.log('Stopped!');
+
+    }
+    //Method for adjusting time interval
+    this.round = () => {
+        let drift = Date.now() - this.expected;
+        // Check if drift is greater than time interval and run err callback if true
+        if(drift > this.timeInterval) {
+            if(options.errorCallback) {
+                options.errorCallback();
+            } 
+        }
+        callback();
+        this.expected += this.timeInterval;
+        console.log(drift);
+        this.timeout = setTimeout(this.round, this.timeInterval - drift);
+    }
+}
+
+export default Timer;
+
