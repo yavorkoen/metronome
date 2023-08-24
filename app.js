7b327e1e9931e19f5fe0c66ebaba4975c16778af
Yavor Koen <yavorkoen@gmail.com>
2023-08-24 09:23:18 +0300

    add eventListeners to elements

--- app.js
+++ app.js
@@ -1,12 +1,46 @@
 // import Timer from "./timer";
 
-const tempoDisplay = document.querySelector('.tempo');
+const tempo = document.querySelector('.tempo');
 const tempoText = document.querySelector('.tempo-text');
-const slider = document.querySelector('.tempo-slider');
-const decreaseTempoBtn = querySelector('.decrease-tempo');
-const increaseTempoBtn = querySelector('.increase-tempo');
-const startStopBtn = querySelector('.start-stop-btn');
-const decreaseBeats = querySelector('.decrease-beats');
-const increaseBeats = querySelector('.increase-beats');
-const measureCount = querySelector('.measure-count');
+const tempoSlider = document.querySelector('.tempo-slider');
+const decreaseTempoBtn = document.querySelector('.decrease-tempo');
+const increaseTempoBtn = document.querySelector('.increase-tempo');
+const startStopBtn = document.querySelector('.start-stop-btn');
+const decreaseBeats = document.querySelector('.decrease-beats');
+const increaseBeats = document.querySelector('.increase-beats');
+const measureCount = document.querySelector('.measure-count');
 
+let bpm = 120;
+let beatsPerMeasure = 4;
+
+decreaseTempoBtn.addEventListener('click', () => {
+    if(bpm <= 40) { return };
+    bpm--;
+    updateMetronome()
+});
+
+increaseTempoBtn.addEventListener('click', () => {
+    if(bpm >= 340) { return };
+    bpm++;
+    updateMetronome()
+});
+
+tempoSlider.addEventListener('input', (e) => {
+    bpm = e.target.value
+    updateMetronome()
+});
+
+decreaseBeats.addEventListener('click', () => {
+    beatsPerMeasure--;
+    measureCount.textContent = beatsPerMeasure;
+});
+increaseBeats.addEventListener('click', () => {
+    if(beatsPerMeasure >= 19) { return };
+    beatsPerMeasure++;
+    measureCount.textContent = beatsPerMeasure;
+});
+
+function updateMetronome() {
+    tempo.textContent = bpm;
+    tempoSlider.value = bpm;
+}
